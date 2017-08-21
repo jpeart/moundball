5de5da2103e1d64875244b4064927b0286a281fe
jpeart <jpeart4@gmail.com>
2017-08-21 14:02:23 -0500

    homepage template

---
+++ assets/javascript/moundball.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBV_fJ1-aRlhaiCTMOi5-0ZHWh8PpLrzNw",
+    authDomain: "moundball-d2fdd.firebaseapp.com",
+    databaseURL: "https://moundball-d2fdd.firebaseio.com",
+    projectId: "moundball-d2fdd",
+    storageBucket: "moundball-d2fdd.appspot.com",
+    messagingSenderId: "132562634718"
+};
+firebase.initializeApp(config);
+
